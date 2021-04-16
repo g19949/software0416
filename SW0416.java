1798390ec286df9099ea2c64ef0b2579d7d44109
g19949 <82643296+g19949@users.noreply.github.com>
2021-04-16 18:29:21 +0900

    Add files via upload

---
+++ SW0416.java
@@ -0,0 +1,8 @@
+public class SW0416{
+  public static void main(String[]args){
+
+   System.out.println("好きな食べ物は明治のピノアイス　アーモンド味です");
+   System.out.println("理由は、アイスの中でも食べやすい一口サイズで、中でもアーモンド味のアイスは珍しいからです。");
+
+  }
+}
